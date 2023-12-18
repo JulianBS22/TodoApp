7e22ab5fa78a9acd0e199258600beba50cd725d7
JulianBS22 <julianbsuarez@hotmail.com>
2023-12-18 20:05:39 +0100

    editar y eliminar tareas

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -8,52 +8,71 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrl: './todo.component.css',
 })
 export class TodoComponent {
-  todolist = signal<TodoModel[]> ([
-    
-      {
-        id: 1,
-        title: 'Comprar leche',
-        completed: false,
-        editing: false,
-      },
-      {
-        id: 2,
-        title: 'Fregar platos',
-        completed: false,
-        editing: false,
-      },
-      {
-        id: 3,
-        title: 'Sacar basura',
-        completed: false,
-        editing: false,
-      },
-    ],
-  );
+  todolist = signal<TodoModel[]>([
+    {
+      id: 1,
+      title: 'Comprar leche',
+      completed: true,
+      editing: false,
+    },
+    {
+      id: 2,
+      title: 'Fregar platos',
+      completed: false,
+      editing: false,
+    },
+    {
+      id: 3,
+      title: 'Sacar basura',
+      completed: false,
+      editing: false,
+    },
+  ]);
   filter = signal<FilterType>('all');
   newTodo = new FormControl('', {
     nonNullable: true,
     validators: [Validators.required, Validators.minLength(3)],
   });
-  changeFilter (filterString: FilterType) {
-    this.filter.set(filterString)
+  changeFilter(filterString: FilterType) {
+    this.filter.set(filterString);
   }
   addTodo() {
     const newTodoTitle = this.newTodo.value.trim();
-    if (this.newTodo.valid && newTodoTitle !== ''){
-      this.todolist.update((prev_todos)=>{
+    if (this.newTodo.valid && newTodoTitle !== '') {
+      this.todolist.update((prev_todos) => {
         return [
           ...prev_todos,
-          {id:Date.now(),title:newTodoTitle, completed: false},
-        ]
-      })
+          { id: Date.now(), title: newTodoTitle, completed: false },
+        ];
+      });
       this.newTodo.reset();
-    }else{
-      this.newTodo.reset()
-  }  
-
+    } else {
+      this.newTodo.reset();
+    }
+  }
+  toogleTodo(todoId: number) {
+    this.todolist.update((prev_todos) =>
+      prev_todos.map((todo) => {
+       return todo.id === todoId? { ...todo, completed: !todo.completed }
+       :todo;
+      })
+    );
+  }
+  removeTodo(todoId: number) {
+    this.todolist.update ((prev_todos) => 
+      prev_todos.filter((todo)=>todo.id !== todoId)
+    )
+  }
+  updateTodoEditingMode(todoId: number) {
+    return this.todolist.update((prev_todos) =>
+      prev_todos.map((todo) => {
+        return todo.id === todoId ?
+        { ...todo, editing: true}:
+        {...todo, editing: false};
+      })
+    )
   }
 }
